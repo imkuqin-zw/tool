339d37333b00e0975e58ca4431ab686b8e6f3c15
zhangwei <zhangwei@kuaifazs.com>
2018-01-31 11:41:26 +0800

    添加 忽略https证书验证

--- net_util/http_util.go
+++ net_util/http_util.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"strings"
 	"io/ioutil"
+	"crypto/tls"
 )
 
 func Get(url string, params url.Values, header, cookie map[string]string) (body []byte, err error) {
@@ -38,7 +39,12 @@ func request(method, url string, params url.Values, header map[string]string, co
 	for k, v := range cookie {
 		req.AddCookie(&http.Cookie{Name: k, Value: v})
 	}
-	client := http.Client{Timeout: 5 * time.Second}
+	// 忽略https证书验证
+	transport := &http.Transport{
+		TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
+		DisableCompression: true,
+	}
+	client := http.Client{Transport: transport, Timeout: 5 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
 		return
